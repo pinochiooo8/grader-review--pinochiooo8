3a471bba1a137640b8ea48133a9fe653b486f4b2
pinochiooo8 <121822382+pinochiooo8@users.noreply.github.com>
2024-03-07 12:48:51 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 rm -rf grading-area
@@ -8,9 +8,25 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [ -f "student-submission/ListExamples.java" ]; then
+  echo "file found"
+else
+  echo "file ListExamples.java not found"
+  exit 1
+fi 
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+cp student-submission/ListExamples.java grading-area/
+cp TestListExamples.java grading-area/
+cp -r lib grading-area/
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+cd grading-area
+javac -cp $CPATH *.java
+
+if [ $? -ne 0 ]; then
+    echo "Compilation error. Please check your submission for errors."
+    exit 1
+fi
+
+
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples
+
